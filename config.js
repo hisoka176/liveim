1ad749eb807ff4ef37ff0fb31ab2bde40701bd50
hisoka176 <hisoka176@sina.com>
2018-10-22 21:55:01 +0800

    接受直播代码

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	'REDISHOST' : '127.0.0.1',
+	'REDISPASS' : 'tieweishivps',
+	'REDISPORT' : '6379',
+	'TOKEN'		: '1234567',
+	'socket_port': '19967',
+	'sensitive_word_path':'./sensitive_word.dict',
+	'WEBADDRESS': 'http://livenew.jingsheng888.cn/api/public/'
+}
